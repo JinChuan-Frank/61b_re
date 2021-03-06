63810d3441ee378cac3104dda3bb849f3de3809f
Jin Chuan <frankjc@126.com>
2021-03-06 19:19:18 +0000

    read planet complete

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -11,7 +11,18 @@ public class NBody{
 		In in = new In(fileName);
 		int numberofPlanets = in.readInt();
 		double radius = in.readDouble();
-		Planet [] pArray = new Planet [3];
+		Planet [] pArray = new Planet [numberofPlanets];
+		for (int i = 0; i < numberofPlanets; i += 1 ){
+			//pArray[i] = new Planet(xP, yP, xV, yV, m, img);
 
+			double xP = in.readDouble();
+			double yP	= in.readDouble();
+			double xV = in.readDouble();
+			double yV = in.readDouble();
+			double m = in.readDouble();
+			String img = in.readString();
+			pArray[i] = new Planet (xP, yP, xV, yV, m, img);
+		}
+		return pArray;
 	}
 }
