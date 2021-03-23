35e819d6155f2f309cf8cbcdfa7260200ce5344a
Your Name <you@example.com>
2021-03-23 17:54:33 +0000

    IntList complete

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -97,6 +97,9 @@ public class IntList {
      * * elements of B.  May NOT modify items of A.  Use 'new'.
      */
     public static IntList catenate(IntList A, IntList B) {
+        if ( A == null){
+            return B;
+        }
         IntList AB = new IntList(A.first,null);
         IntList p = AB;
         IntList L = A.rest;
