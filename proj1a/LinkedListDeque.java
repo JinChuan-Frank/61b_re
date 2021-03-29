49731ea591f2e492a252f1318d6184f5ec6aaee1
Jin Chuan <frankjc@126.com>
2021-03-29 23:10:10 +0000

    move on from removeFirst

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -10,43 +10,68 @@ public class LinkedListDeque {
         }
     }
 
-    private IntNode first;
-    private IntNode last;
+    private IntNode sentFront;
+    private IntNode sentBack;
     private int size;
 
     public LinkedListDeque(int x) {
-        first = new IntNode(x, null, null);
-        last =  new IntNode(x, null, null);
+        sentFront = new IntNode (10, null, null);
+        sentBack = new IntNode (-10, null, null);
+        sentFront.next = new IntNode (x, sentFront, sentBack);
+        sentBack.previous = sentFront.next;
         size = 1;
     }
 
     public LinkedListDeque(){
-        first = null;
-        last = null;
+        sentFront = new IntNode (10, null, null);
+        sentBack = new IntNode (-10, null, null);
         size = 0;
     }
 
     /** Adds an item to the front of the list. */
     public void addFirst(int x) {
-        first = new IntNode(x, null, first);
+        sentFront.next = new IntNode (x, sentFront, sentFront.next);
+        sentFront.next.next.previous = sentFront.next;
         size += 1;
     }
 
-    /** Retrieves the front item from the list. */
-    public int getFirst() {
-        return first.item;
-    }
 
     /** Adds an item to the end of the list. */
     public void addLast(int x) {
-        last = new IntNode(x, last, null);
+        sentBack.previous = new IntNode (x, sentBack.previous, sentBack);
+        sentBack.previous.previous.next =  sentBack.previous;
         size += 1;
+    }
+
+    /**Returns true if deque is empty, false otherwise.*/
+    public boolean isEmpty(){
+        if (size = 0) {
+            return true;
+        } else{
+            return false;
+        }
     }
 
     /** Returns the number of items in the list using recursion. */
     public int size() {
         return size;
     }
+
+    /**Prints the items in the deque from first to last, separated by a space. */
+    public void printDeque() {
+        for (IntNode p = sentFront.next; p != sentBack; p = p.next) {
+            System.out.print(p.item + " ");
+        }
+    }
+
+
+    public int removeFirst() {
+        sentFront.next = sentFront.next.next;
+        sentFront.next.previous = sentFront;
+        return sentFront.next.item;
+    }
+
+
 
 
 
