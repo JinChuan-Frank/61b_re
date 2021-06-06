5c3dcfe8cc7c6f43a81eec854374f7b13426f9fb
Jin Chuan <frankjc@126.com>
2021-06-06 22:58:01 +0000

    add Hexagon

--- proj2/byog/lab5/HexWorld.java
+++ proj2/byog/lab5/HexWorld.java
@@ -12,4 +12,30 @@ import java.util.Random;
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
+    private static final int WIDTH = 50;
+    private static final int HEIGHT = 50;
+
+    public static void addHexgagon() {
+
+    }
+
+    /**
+     * Computes the width of row i for a size s hexagon.
+     * @param s The size of the hex.
+     * @param i The row number where i = 0 is the bottom row.
+     * @return
+     */
+    public static int hexRowWidth(int s, int i) {
+        int rowwidth = s;
+        if ( i < s) {
+
+        }
+        return rowwidth;
+    }
+
+    public static void main(String[] args) {
+        TERenderer ter = new TERenderer();
+        ter.initialize(WIDTH, HEIGHT);
+    }
+
 }
