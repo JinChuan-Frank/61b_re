6d05d672125f70fc70e86af58cd832455cdad1b9
Your Name <you@example.com>
2022-04-29 18:20:09 +0000

    Implementing StrangeBitwiseGenerator

--- lab14/lab14/Main.java
+++ lab14/lab14/Main.java
@@ -5,7 +5,7 @@ import lab14lib.GeneratorAudioVisualizer;
 
 public class Main {
 	public static void main(String[] args) {
-		Generator generator = new AcceleratingSawToothGenerator(200, 0.95);
+		Generator generator = new StrangeBitwiseGenerator(200);
 		GeneratorAudioVisualizer gav = new GeneratorAudioVisualizer(generator);
 		gav.drawAndPlay(1000, 1000);
 	}
---
+++ lab14/lab14/StrangeBitwiseGenerator.java
@@ -0,0 +1,30 @@
+package lab14;
+
+import lab14lib.Generator;
+
+public class StrangeBitwiseGenerator implements Generator {
+    private int period;
+    private int state;
+
+    public StrangeBitwiseGenerator(int period) {
+        state = 0;
+        this.period = period;
+    }
+
+    @Override
+    public double next() {
+        state = state + 1;
+        int weirdState = state & (state >>> 3) % period;
+        return normalize(weirdState);
+    }
+
+    private double normalize(int state) {
+        int x = state % period;
+        if (x == 0) {
+            return -1.0;
+        }
+        double unitIncrease = 2.0 / period;
+        double y = -1.0 + x * unitIncrease;
+        return y;
+    }
+}
