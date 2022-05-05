58913acba818b084f0d78e9a789d6bd3094198a3
Your Name <you@example.com>
2022-05-05 18:29:46 +0000

    Implementing SeamCarver

--- hw5/SeamCarver.java
+++ hw5/SeamCarver.java
@@ -1,5 +1,75 @@
+import edu.princeton.cs.algs4.Picture;
+
 public class SeamCarver {
+
     public SeamCarver(Picture picture) {
 
     }
+
+    /**
+     *
+     * @return current picture
+     */
+    public Picture picture() {
+        return null;
+    }
+
+    /**
+     *
+     * @return width of current picture
+     */
+    public int width() {
+        return 0;
+    }
+
+    /**
+     *
+     * @return height of current picture
+     */
+    public int height() {
+        return 0;
+    }
+
+    /**
+     *
+     * @param x
+     * @param y
+     * @return energy of pixel at column x and row y
+     */
+    public double energy(int x, int y) {
+        return 0.0;
+    }
+
+    /**
+     *
+     * @return sequence of indices for horizontal seam
+     */
+    public int[] findHorizontalSeam() {
+        return null;
+    }
+
+    /**
+     *
+     * @return sequence of indices for vertical seam
+     */
+    public int[] findVerticalSeam() {
+        return null;
+    }
+
+    /**
+     * remove horizontal seam from picture
+     * @param seam
+     */
+    public void removeHorizontalSeam(int[] seam) {
+
+    }
+
+    /**
+     * remove vertical seam from picture
+     * @param seam
+     */
+    public void removeVerticalSeam(int[] seam) {
+
+    }
+
 }
