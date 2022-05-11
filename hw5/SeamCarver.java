bef5e55c5a41e80dcd751c26f2c9ccd20c994ff6
Your Name <you@example.com>
2022-05-11 18:30:43 +0000

    Implementing SeamCarver

--- hw5/SeamCarver.java
+++ hw5/SeamCarver.java
@@ -46,6 +46,25 @@ public class SeamCarver {
         return 0.0;
     }
 
+    private double calXGradient(int x, int y) {
+
+        int leftX = x - 1;
+        int rightX = x + 1;
+        if (leftX < 0) {
+            leftX = image.width() - 1;
+        }
+        if (rightX == image.width()) {
+            rightX = 0;
+        }
+        Color leftNeighbor = image.get(leftX, y);
+        Color rightNeighbor = image.get(rightX, y);
+        int leftRGB = leftNeighbor.getRGB();
+        int r1 = (leftRGB >> 16) & 0xFF;
+        int g1 = (leftRGB >>  8) & 0xFF;
+        int b1 = (leftRGB >>  0) & 0xFF;
+        return 0.0;
+    }
+
     /**
      *
      * @return sequence of indices for horizontal seam
